41d6d5ed4320affa989d4ba382b8290b8edf46ed
OBrien-reece <indeche.evans.o@gmail.com>
2021-04-27 21:37:36 +0300

    2st Test

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row">
+       <div class="col-3">
+           <img src="/testsvg/Luffy.jpg" style="height: 200px;width: 200px" class="rounded-circle p-3">
+       </div>
+       <div class="col-9">
+           <div><h2>_thatboy_obrien</h2></div>
+           <div class="d-flex">
+           <div class="pr-5"><strong>153</strong> posts</div>
+           <div class="pr-5"><strong>1,344</strong> followers</div>
+           <div class="pr-5"><strong>1,234</strong> following</div>
+           </div>
+               <div class="pt-3 font-weight-bold">Pandoras Box</div>
+               <div>♠️Just bekoz u a shooter dont make u a man♠️</div>
+               <div>☢️It's how u play ua hand or react when u in that jam☢️</div>
+               <div><a href="#">www.alcatrez.org</a></div>
+          </div>
+            <div class="col-md-9 m-auto d-flex pt-4">
+                <img class="pr-2" src="/testsvg/e.jpg" style="width: 250px" class="w-100">
+                <img class="pr-2" src="/testsvg/q.jpg" style="width: 250px" class="w-100">
+                <img class="pr-2" src="/testsvg/w.jpg" style="width: 250px" class="w-100">
+            </div>
+       
+   </div>
+</div>
+@endsection
